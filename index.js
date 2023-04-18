f39c459fa8b5648b294d25de61837e166fe41559
mmatczak <marek.matczak79@gmail.com>
2023-04-18 13:48:03 +0200

    promises

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-const myElements = [1, new Date(), 'Marek', false, true, {name: 'Marek'}, a => -a];
-let myNumbers = [1, 45, 67, 77, 23, 45];
-
-
-let sum = 0;
-for (let i = 0; i < myNumbers.length; i++) {
-    const taxedValue = addTaxOf(5)(myNumbers[i]);
-    const onlyImportantValues = onlyImportantValuesWithThreshold(30);
-    if (onlyImportantValues(taxedValue)) {
-        sum = calculateSum(sum, taxedValue);
-    }
-}
-console.log(sum);
-const importanceThreshold = 30;
-const calculatedSum = myNumbers
-    .map(addTaxOf(5))
-    .filter(onlyImportantValuesWithThreshold(importanceThreshold))
-    .reduce(calculateSum);
-
-console.log(calculatedSum);
-console.log(add(1, 2, 4, 5, 6, 666, 6));
-
-function calculateSum(acc, currentValue) {
-    return acc + currentValue;
-}
-
-// const importanceThreshold = 30;
-function onlyImportantValuesWithThreshold(importanceThreshold) {
-    return function (value) {
-        return value > importanceThreshold;
+class OrderService {
+    findById(id) {
+        return new Promise((resolve, reject) => {
+            setTimeout(function () {
+                if (id < 0) {
+                    reject(new Error('Hahah'));
+                } else {
+                    resolve({
+                        id, businessKey: 'ABC123'
+                    });
+                }
+            }, 2000);
+        });
     }
 }
 
-function addTaxOf(taxValue) {
-    return function addTax(value) {
-        return value + taxValue;
+const orders = new OrderService();
+doIt();
+
+console.log('End');
+
+async function doIt() {
+    try {
+        const order = await orders.findById(123);
+        console.log(order);
+    } catch (e) {
+        console.log('There is an error....');
+        throw e;
     }
 }
 
-function add() {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
-    }
-    return sum;
-}
 
