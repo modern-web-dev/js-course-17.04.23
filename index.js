d7097f8b3bb3ba4e546a4af38a8954b971bf0c12
mmatczak <marek.matczak79@gmail.com>
2023-04-18 10:44:51 +0200

    functions

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-class Person {
-    constructor(firstName, lastName, age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-}
-const marek = new Person('Marek', 'Matczak', 0);
-
-const czarek = {
-    firstName: 'Czarek',
-    lastName: 'Kowalski',
-    age: 33
-}
-
-const marysia = {
-    'first-name': 'Marysia',
-    lastName: 'Nowak',
-    age: 35,
-    sex: 'female',
-    address: {
-        zipCode: '53-601',
-        streetName: 'Legnicka',
-        city: 'Wroclaw'
+const person = {
+    firstName: 'Marek',
+    age: 44,
+    whoIAm() {
+        // return 'My name is ' + this.firstName;
+        return `My name is ${this.firstName.toUpperCase()}
+        ...and I am ${this.age} years old`;
     }
 }
 
-marek.favouriteColor = 'red';
-// delete marek.age;
-
-// falsy values: undefined, null, '', 0, false, NaN
-// truthy values: {}, []
-
-const zipCode = marek?.age ?? 'no age';
-
-if(zipCode != null) {
-
-}
-
-console.log(zipCode);
+console.log(person.whoIAm());
