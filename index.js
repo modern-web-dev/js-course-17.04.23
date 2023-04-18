a903c3e064247e4a64a7574e7bbe26d382f41b6f
mmatczak <marek.matczak79@gmail.com>
2023-04-18 12:02:57 +0200

    arrays

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const person = {
-    firstName: 'Marek',
-    age: 44,
-    whoIAm() {
-        // return 'My name is ' + this.firstName;
-        return `My name is ${this.firstName.toUpperCase()}
-        ...and I am ${this.age} years old`;
+const myElements = [1, new Date(), 'Marek', false, true, {name: 'Marek'}, a => -a];
+let myNumbers = [1, 45, 67, 77, 23, 45];
+
+
+let sum = 0;
+for (let i = 0; i < myNumbers.length; i++) {
+    const taxedValue = addTaxOf(5)(myNumbers[i]);
+    const onlyImportantValues = onlyImportantValuesWithThreshold(30);
+    if (onlyImportantValues(taxedValue)) {
+        sum = calculateSum(sum, taxedValue);
+    }
+}
+console.log(sum);
+const importanceThreshold = 30;
+const calculatedSum = myNumbers
+    .map(addTaxOf(5))
+    .filter(onlyImportantValuesWithThreshold(importanceThreshold))
+    .reduce(calculateSum);
+
+console.log(calculatedSum);
+console.log(add(1, 2, 4, 5, 6, 666, 6));
+
+function calculateSum(acc, currentValue) {
+    return acc + currentValue;
+}
+
+// const importanceThreshold = 30;
+function onlyImportantValuesWithThreshold(importanceThreshold) {
+    return function (value) {
+        return value > importanceThreshold;
     }
 }
 
-console.log(person.whoIAm());
+function addTaxOf(taxValue) {
+    return function addTax(value) {
+        return value + taxValue;
+    }
+}
+
+function add() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+
