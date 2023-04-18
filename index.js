6d14a70ee1c2364a8a4430b820352dd09a9b3acd
mmatczak <marek.matczak79@gmail.com>
2023-04-18 10:19:12 +0200

    objects

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
+const marek = new Person('Marek', 'Matczak', 0);
+
+const czarek = {
+    firstName: 'Czarek',
+    lastName: 'Kowalski',
+    age: 33
+}
+
+const marysia = {
+    'first-name': 'Marysia',
+    lastName: 'Nowak',
+    age: 35,
+    sex: 'female',
+    address: {
+        zipCode: '53-601',
+        streetName: 'Legnicka',
+        city: 'Wroclaw'
+    }
+}
+
+marek.favouriteColor = 'red';
+// delete marek.age;
+
+// falsy values: undefined, null, '', 0, false, NaN
+// truthy values: {}, []
+
+const zipCode = marek?.age ?? 'no age';
+
+if(zipCode != null) {
+
+}
+
+console.log(zipCode);
